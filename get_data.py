e270920274ff2952ac1737bf620f1efcb819cc2d
Rafael García Álvarez <vattenbit@gmail.com>
2023-01-21 00:09:52 +0100

    Added file for speed if was created less as 24 hours

--- get_data.py
+++ get_data.py
@@ -4,6 +4,7 @@ import argparse
 import requests
 import json
 import os
+import datetime
 from flask import Flask, request
 from pprint import pprint
 
@@ -12,11 +13,28 @@ with open("/run/secrets/do_token", 'r') as f: DO_TOKEN = f.read().splitlines()[0
 
 app = Flask(__name__)
 
+@app.route('/digitalocean')
+def main():
+    return "Workflow Engine API for DigitalOcean"
+
 @app.route('/digitalocean/<data_type>')
 def get_data(data_type):
+    if data_type not in ['regions','size','images']: 
+        return "Workflow Engine API for DigitalOcean"
     style = request.args.get('style')
-    data = get_data(data_type)
-    if style == "key/value":
+    reload = request.args.get('reload')
+    if reload: reload = reload.lower() == 'true' or reload.lower() == '1'
+    data = get_api_data(data_type)
+    file_data = f"{data_type}_{style}.json" if style else f"{data_type}.json"
+    
+    if os.path.isfile(file_data):
+        modification_time = datetime.datetime.fromtimestamp(os.path.getmtime(file_data))
+        if modification_time > (datetime.datetime.now()-datetime.timedelta(hours=24)) and not reload:
+            with open(file_data, 'r') as file:
+                return json.load(file)
+
+    output_json = data
+    if style == "rundeck":
         output_json = {}
         ord = 1
         for item in data.get(data_type):
@@ -35,10 +53,12 @@ def get_data(data_type):
                     key = item.get('name')
                     value = item.get('slug')
             output_json[key] = value
-        return output_json
-    return data
 
-def get_data(type_data):
+    with open(file_data, 'w') as file:
+        file.write(json.dumps(output_json))
+        return output_json
+
+def get_api_data(type_data):
     headers = {'content-type': 'application/json', 'Authorization': 'Bearer {0}'.format(DO_TOKEN)}
     req = requests.get(URL_API+"/"+type_data+"?per_page=500", headers=headers)
     response = req.json()
